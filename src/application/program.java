73109c7b2c55174baac29c4647eb580ba7e1ebb1
josivan <josivan@JosevanDELL>
2019-05-16 13:17:03 -0300

    correcao

--- src/application/program.java
+++ src/application/program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int n = sc.nextInt();
+		
+
+		sc.close();
 	}
 
 }
