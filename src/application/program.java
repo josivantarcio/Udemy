1897677940562629448490a3d1b32a7ca19066ea
josivan <josivan@JosevanDELL>
2019-05-18 01:15:41 -0300

    2

--- src/application/program.java
+++ src/application/program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class program {
 
 	public static void main(String[] args) {
@@ -10,6 +12,22 @@ public class program {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		Product[] vetor = new Product[n];
+		double soma=0;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("Produto: ");
+			String nome = sc.next();
+			System.out.print("Preço: ");
+			double preco = sc.nextDouble();
+			
+			vetor[i] = new Product(nome,preco);
+			soma = vetor[i].getPreco() + soma;
+			System.out.println(soma);
+			System.out.println(vetor[i].toString());
+		}
+		System.out.println(soma);
+		System.out.println("Media: "+ soma / n);
 		
 
 		sc.close();
