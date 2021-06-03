6e314fc804a97b1eb647b5d593504a1774a7eda1
Josevan Oliveira <josivantarcio@gmail.com>
2021-06-03 16:50:44 -0300

    Secao 04-4.41b

---
+++ Geek-Java/src/geek/Programa09.java
@@ -0,0 +1,25 @@
+package geek;
+
+import java.util.Scanner;
+
+//explicando o FOR
+public class Programa09 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int idade;
+		String nome;
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print("Nome: ");
+			nome = scan.nextLine();
+
+			System.out.print("Idade: ");
+			idade = Integer.parseInt(scan.nextLine());
+			
+			System.out.println("nome "+nome+" idade: "+idade);
+		}
+		scan.close();
+		System.out.println("FIM");
+	}
+}
