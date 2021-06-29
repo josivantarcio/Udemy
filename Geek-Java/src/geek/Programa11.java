b2727dc419602c88a3010c7e3b029613f7991e8a
Josevan Oliveira <josivantarcio@gmail.com>
2021-06-29 15:52:55 -0300

    testes do curso mod7

---
+++ Geek-Java/src/geek/Programa11.java
@@ -0,0 +1,25 @@
+package geek;
+
+public class Programa11 {
+	public static void main(String[] args) {
+		
+		System.out.println(Long.SIZE);
+		System.out.println(Integer.SIZE);
+		System.out.println(Short.SIZE);
+		System.out.println(Byte.SIZE);
+		System.out.println(Character.SIZE);
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		
+	}
+}
---
+++ Geek-Java/src/geek/Programa12.java
@@ -0,0 +1,20 @@
+//vetores parte 1
+package geek;
+
+public class Programa12 {
+
+	public static void main(String[] args) {
+		//Arrays
+		int numeros[] = new int[5]; //Declara o vetor e especifica o tamanho
+		int numero[] = {1, 3, 5, 7, 9}; //Declara o vetor e  inicia
+		int num []; //Declara somente o vetor
+		
+		float preco[] = {12.4f, 25.5f, 18.78f}; //vetor com ponto flutuante
+		String frutas[] = {"Goiaba","Uva","Pera"}; //vetor com String
+		boolean chaves[] = {true,false,false,true,true}; //vetor com bolleano
+		
+		System.out.println(preco[2]);
+
+	}
+
+}
