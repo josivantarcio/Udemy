df9ba9cffd8db2e0e359163130660524d516ef09
Josevan Oliveira <josivantarcio@gmail.com>
2021-06-29 18:16:49 -0300

    Create Programa17.java

---
+++ Geek-Java/src/geek/Programa17.java
@@ -0,0 +1,29 @@
+package geek;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Programa17 {
+
+	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		Random aleatorio = new Random();
+//
+//		System.out.print("Quant de Numeros: ");
+//		int num = scan.nextInt();
+//		int vetor[] = new int[num];
+//
+//		for (int i = 0; i < vetor.length; i++) {
+//			vetor[i] = aleatorio.nextInt(9999);
+//		}
+//		for (int i = 0; i < vetor.length; i++) {
+//			System.out.print(vetor[i]+" ");
+//		}
+//		
+		System.out.println("Numero gerado: " + (int)(Math.random()*10));
+
+//		scan.close();
+
+	}
+
+}
