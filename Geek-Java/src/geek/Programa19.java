f86be5edec8ed3f3f7a896c213ab97899c42549a
Josevan Oliveira <josivantarcio@gmail.com>
2021-07-15 21:43:42 -0300

    matriz-aula

---
+++ Geek-Java/src/geek/Programa19.java
@@ -0,0 +1,23 @@
+package geek;
+
+public class Programa19 {
+	public static void main(String[] args) {
+		int num[][] = new int[3][3];
+
+		num[0][0] = 1;
+		num[0][1] = 3;
+		num[0][2] = 5;
+		num[1][0] = 7;
+		num[1][1] = 9;
+		num[1][2] = 11;
+		num[2][0] = 13;
+		num[2][1] = 15;
+		num[2][2] = 17;
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num[i].length; j++) {
+				System.out.println(num[i][j]);
+			}
+		}
+	}
+}
