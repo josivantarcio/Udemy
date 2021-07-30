fc5afa7277d7c1348dcf44457bc0c2fd7691450f
Josevan Oliveira <josivantarcio@gmail.com>
2021-07-30 17:43:24 -0300

    Secao 07-29.6

--- Geek-Java/src/exercicios/secao07/Exercicio21.java
+++ Geek-Java/src/exercicios/secao07/Exercicio21.java
@@ -1,10 +1,31 @@
 package exercicios.secao07;
 
+import java.util.Scanner;
+
 public class Exercicio21 {
 
 	public static void main(String[] args) {
-		
-		
+		Scanner scan = new Scanner(System.in);
+		int a[] = new int[10];
+		int b[] = new int[10];
+		int c[] = new int[10];
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Valor de A na Posição " + (i + 1) + ": ");
+			a[i] = scan.nextInt();
+
+			System.out.println("Valor de B na Posição " + (i + 1) + ": ");
+			b[i] = scan.nextInt();
+
+			c[i] = a[i] - b[i];
+		}
+
+		for (int i = 0; i < c.length; i++) {
+			System.out.println("Calculo " + (1 + i) + ": " + c[i]);
+		}
+
+		scan.close();
+
 	}
 
 }
---
+++ Geek-Java/src/exercicios/secao07/Exercicio29.java
@@ -0,0 +1,29 @@
+package exercicios.secao07;
+
+import java.util.Random;
+
+public class Exercicio29 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int num[] = new int[6], pares=0, impares=0;
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (int) rand.nextInt(9);
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] % 2 == 0) {
+				System.out.println("Pares: " + num[i]);
+				pares += num[i];
+			}else {
+				System.out.println("Impares: "+num[i]);
+				impares += num[i];
+			}
+
+		}
+		System.out.println("Soma dos pares "+pares);
+		System.out.println("Soma dos Impares "+impares);
+
+	}
+
+}
