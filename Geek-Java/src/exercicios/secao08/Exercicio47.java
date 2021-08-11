48a64e2c83c9edd8a07c9a4c17e7ad0dd58f8a13
Josevan Oliveira <josivantarcio@gmail.com>
2021-08-10 22:11:50 -0300

    Secao 08-54.4

--- Geek-Java/src/exercicios/secao08/Exercicio47.java
+++ Geek-Java/src/exercicios/secao08/Exercicio47.java
@@ -1,13 +1,12 @@
 package exercicios.secao08;
 
 import java.util.Random;
-
 import javax.swing.JOptionPane;
 
 public class Exercicio47 {
 	public static void main(String[] args) {
 
-		JOptionPane.showMessageDialog(null, "Quantidade: ");
+		JOptionPane.showMessageDialog(null, "Quantidade: "+matriz());
 	}
 
 	private static int matriz() {
@@ -17,9 +16,10 @@ public class Exercicio47 {
 		int[][] mat = new int[4][4];
 		
 		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
 				n = rad.nextInt(20);
 				mat[i][j]  = n; 
+				System.out.print(mat[i][j]+" ");
 				if (mat[i][j] > 10) {
 					maior++;
 				}
