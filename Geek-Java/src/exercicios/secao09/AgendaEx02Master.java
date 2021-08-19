307baa667f58e3d660d69c131af4331000213a0c
Josevan Oliveira <josivantarcio@gmail.com>
2021-08-19 15:47:54 -0300

    Update AgendaEx02Master.java

--- Geek-Java/src/exercicios/secao09/AgendaEx02Master.java
+++ Geek-Java/src/exercicios/secao09/AgendaEx02Master.java
@@ -7,7 +7,37 @@ public class AgendaEx02Master {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		AgendaEx02 contato = new AgendaEx02();
+		String nome;
+		int idade;
+		float altura;
 
+		for (int i = 0; i < contato.getPessoas(); i++) {
+			System.out.print("Nome: ");
+			nome = scan.next();
+
+			System.out.print("Idade: ");
+			idade = scan.nextInt();
+
+			System.out.print("Altura: ");
+			altura = scan.nextFloat();
+
+			contato.armazenarPessoas(nome, idade, altura);
+		}
+		
+//		System.out.print("Remover qual nome: ");
+//		contato.removerPessoas(scan.next());
+		
+		
+		System.out.println("Qual a posição de...");
+		contato.buscarPessoa(scan.next());
+		
+		
+		System.out.println("Imprimir Agenda");
+		contato.imprimirAgenda();
+		
+		System.out.println();
+		
+		
 		
 	}
 
