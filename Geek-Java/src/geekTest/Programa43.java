7443f10ea4270860812c87074ebdf21c35eea4a4
Josevan <josivantarcio@msn.com>
2021-10-04 16:23:46 -0300

    testando as classes em aula

--- Geek-Java/src/geekTest/Programa43.java
+++ Geek-Java/src/geekTest/Programa43.java
@@ -1,23 +1,19 @@
 package geekTest;
 
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Programa43 {
 	public static void main(String[] args) {
 		try {
-			Scanner scan = new Scanner(new FileInputStream("boatarde.doc"));
-			int i = 0;
-			while(scan.hasNextLine()) { 
-				System.out.println(scan.nextLine());
-				i++;
-			}
-			 System.out.println("Quantidade de linhas: "+ i);
-		}catch(FileNotFoundException e) {
-			System.out.println("Arquivo Não Encontrado");
+			BufferedReader br = new BufferedReader(new FileReader("arq.txt"));
 			
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo nao encontrado");
 		}
 		
 	}
